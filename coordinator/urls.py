584e27bd15feccc6d508f51cba2bba94130393af
surya <suryaparsa@gmail.com>
2018-08-03 11:33:17 +0530

    Add template for coordinator and presenter for landing page

--- coordinator/urls.py
+++ coordinator/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from coordinator import views
-#import eventcoord
+
+# import eventcoord
 
 urlpatterns = [
-	path(r'^$', views.main_panel, name='coord_main_panel'),
-	path(r'^(?P<event_id>\d+)/dashboard/$', views.dashboard, name='coord_dashboard'),
+    path(r'^$', views.main_panel, name='coord_main_panel'),
+    path(r'^(?P<event_id>\d+)/dashboard/$', views.dashboard, name='coord_dashboard'),
 
 ]
--- coordinator/views.py
+++ coordinator/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, reverse
 
-# Create your views here.
+from event.models import Event
+
+
 def main_panel(request):
     """Main panel for coordinator."""
-    pass
+
+    # check if the user is authenticated
+    if request.user.is_authenticated:
+        custom_user = request.user
+
+        if custom_user.is_admin:
+            event_list = Event.objects.all()
+            # flag is used to give option for the user to the edit event
+            return render(request, "coordinator/index.html", {'event_list': event_list, 'flag': True})
+
+        if custom_user.is_ecoord:
+            event_list = custom_user.event_set.all()
+            # flag is used to give option for the user to the edit event
+            return render(request, "coordinator/index.html", {'event_list': event_list, 'flag': True})
+
+        if custom_user.is_presenter:
+            event_list = custom_user.event_set.all()
+            # No flag is set here since presenter don't have an option to edit the event
+            return render(request, "coordinator/index.html", {'event_list': event_list})
+    else:
+        # TODO: Redirect to login page
+        return HttpResponse("Login Page")
+
 
 def dashboard(request):
     """dashboard for coordinator."""
