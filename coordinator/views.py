69ca516a9f94dba888f3cd021b4192958e44cc6d
surya <suryaparsa@gmail.com>
2018-08-03 11:38:26 +0530

    update views.py to render the template when the user is coordinator

--- coordinator/views.py
+++ coordinator/views.py
@@ -15,16 +15,9 @@ def main_panel(request):
             event_list = Event.objects.all()
             # flag is used to give option for the user to the edit event
             return render(request, "coordinator/index.html", {'event_list': event_list, 'flag': True})
-
-        if custom_user.is_ecoord:
-            event_list = custom_user.event_set.all()
-            # flag is used to give option for the user to the edit event
-            return render(request, "coordinator/index.html", {'event_list': event_list, 'flag': True})
-
-        if custom_user.is_presenter:
-            event_list = custom_user.event_set.all()
-            # No flag is set here since presenter don't have an option to edit the event
-            return render(request, "coordinator/index.html", {'event_list': event_list})
+        else:
+            # TODO: Redirect to login page
+            return HttpResponse("Login Page")
     else:
         # TODO: Redirect to login page
         return HttpResponse("Login Page")
