1b736257c49d63209acbc9b00883915e3a04787d
pm17 <parveshmourya96@gmail.com>
2018-08-05 23:36:24 +0530

    Added a few screens

--- event/urls.py
+++ event/urls.py
@@ -3,7 +3,7 @@ from event import views
 #import eventcoord
 
 urlpatterns = [
-	url(r'create/^$', views.create_event, name='create_event'),
+	url(r'create/$', views.create_event, name='create_event'),
 	url(r'^(?P<event_id>\d+)/edit/$', views.edit_event, name='edit_event'),
 	url(r'^(?P<event_id>\d+)/details/$', views.view_event, name='view_event'),
 	url(r'^(?P<event_id>\d+)/qr/$', views.show_event_qr, name='view_event_qr'),
